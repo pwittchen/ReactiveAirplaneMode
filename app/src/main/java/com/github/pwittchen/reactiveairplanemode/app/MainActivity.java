c4a6e0a62e86676bd6d09ad5e2ae5f6933b869e6
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-08-14 13:41:44 +0200

    adding project skeleton

---
+++ app/src/main/java/com/github/pwittchen/reactiveairplanemode/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.pwittchen.reactiveairplanemode.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.github.pwittchen.reactiveairplanemode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
---
+++ library/src/test/java/reactiveairplanemode/pwittchen/github/com/library/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package reactiveairplanemode.pwittchen.github.com.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ */
+@RunWith(RobolectricTestRunner.class) public class ExampleUnitTest {
+
+  @Test public void additionShouldBeCorrect() throws Exception {
+    assertThat(1 + 3).isEqualTo(4);
+  }
+}
